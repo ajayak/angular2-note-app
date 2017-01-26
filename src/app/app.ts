d7b150414d42a301b19af168e808b336ea00e00a
Ajay Kumar <ajayak731@hotmail.com>
2017-01-26 20:25:41 +0530

    added first component

---
+++ src/app/app.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app',
+    template: `
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    `
+})
+export class App { }
