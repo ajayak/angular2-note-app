61c8008fb0b5fe37da7b9a559865743637f16336
Ajay Kumar <ajayak731@hotmail.com>
2017-01-26 21:17:55 +0530

    added ui app-bar component

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { App } from './app';
 import { Main } from './app/containers';
+import { AppBar } from './app/ui';
 
 @NgModule({
     declarations: [
         App,
-        Main
+        Main,
+        AppBar
     ],
     imports: [BrowserModule],
     bootstrap: [App]
