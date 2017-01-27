f34f5cce2b1a35b7be9f36bcda6e916cb5b6b7bc
Ajay Kumar <ajayak731@hotmail.com>
2017-01-27 11:13:29 +0530

    created API service

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { App } from './app';
+import { App, providers } from './app';
 import { Main, NotesContainer } from './app/containers';
 import { AppBar, NoteCard, NoteCreator, ColorPicker } from './app/ui';
 
@@ -17,8 +18,9 @@ import { AppBar, NoteCard, NoteCreator, ColorPicker } from './app/ui';
         NoteCreator,
         ColorPicker
     ],
-    imports: [BrowserModule, FormsModule],
-    bootstrap: [App]
+    imports: [BrowserModule, FormsModule, HttpModule],
+    bootstrap: [App],
+    providers
 })
 export class AppModule { }
 
