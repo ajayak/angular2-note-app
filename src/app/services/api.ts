57ce4025219844ed77fe2aec56fcfa6b38749198
Ajay Kumar <ajayak731@hotmail.com>
2017-01-27 11:17:48 +0530

    added noteservice

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -15,7 +15,7 @@ export class ApiService {
         'Accept': 'application/json'
     });
 
-    api_url: string = 'http://localhost:3500/';
+    api_url: string = 'http://localhost:3500';
 
     constructor(private http: Http) { }
 
